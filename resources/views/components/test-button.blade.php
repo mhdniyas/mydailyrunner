b29da62eded8a2fe962caaf3249fd7d40d3119c9
root <mhdniyas37@gmail.com>
2025-06-10 18:38:49 +0000

    Update: auto-commit changes

---
+++ resources/views/components/test-button.blade.php
@@ -0,0 +1,3 @@
+<div style="position: fixed; bottom: 20px; right: 20px; width: 60px; height: 60px; background-color: red; color: white; border-radius: 50%; display: flex; justify-content: center; align-items: center; z-index: 9999999; cursor: pointer;" onclick="alert('Calculator clicked!')">
+    CALC
+</div>
